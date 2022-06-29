e54b43dc3f527d5e5e0a544c982f66c15811a95c
serikmeiirbek <serik.meiirbek@gmail.com>
2022-06-29 09:29:26 -0400

    UAdded Controllers and added dependencies in pox.xml

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -22,22 +23,26 @@ public class ProjectController {
     }
 
     @GetMapping
+    @RolesAllowed({"Admin", "Manager"})
     public ResponseEntity<ResponseWrapper> getProjects() {
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectService.listAllProjects(), HttpStatus.OK));
     }
 
     @GetMapping("/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectsByCode(@PathVariable String projectCode) {
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully retrieved", projectService.getByProjectCode(projectCode), HttpStatus.OK));
     }
 
     @PostMapping()
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project) {
         projectService.save(project);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is created", HttpStatus.CREATED));
     }
 
     @PostMapping("/update")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project) {
         projectService.update(project);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully updated", project, HttpStatus.OK));
@@ -45,12 +50,14 @@ public class ProjectController {
     }
 
     @DeleteMapping("/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectCode") String projectcode) {
         projectService.delete(projectcode);
         return ResponseEntity.ok(new ResponseWrapper("Project is created", HttpStatus.OK));
     }
 
     @GetMapping("/manager/project-status")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectByManager() {
         List<ProjectDTO> projects = projectService.listAllProjectDetails();
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projects, HttpStatus.OK));
@@ -58,6 +65,7 @@ public class ProjectController {
     }
 
     @PostMapping("/manager/complete/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode){
         projectService.complete(projectCode);
         return ResponseEntity.ok(new ResponseWrapper("Project is completed", HttpStatus.OK));
--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RequestMapping("/api/v1/task")
@@ -21,34 +22,40 @@ public class TaskController {
     }
 
     @GetMapping()
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getTasks(){
         return ResponseEntity.ok(new ResponseWrapper("Tasks are successfully retrieved", taskService.listAllTasks(), HttpStatus.OK));
     }
 
     @GetMapping("/{taskId}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getTaskById(@PathVariable("taskId") Long taskId){
         return ResponseEntity.ok(new ResponseWrapper("Task is successfully retrieved", taskService.findById(taskId), HttpStatus.OK));
     }
 
     @PostMapping()
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> createTask(@RequestBody TaskDTO task){
         taskService.save(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Task is created", HttpStatus.CREATED));
     }
 
     @DeleteMapping("TaskId")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> deleteTasks(@PathVariable Long taskId){
         taskService.delete(taskId);
         return ResponseEntity.ok(new ResponseWrapper("Task is deleted", HttpStatus.OK));
     }
 
     @PutMapping()
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> updateTasks(@RequestBody TaskDTO task){
         taskService.update(task);
         return ResponseEntity.ok(new ResponseWrapper("Task is successfully updated", task, HttpStatus.OK));
     }
 
     @GetMapping("/employee/pending-tasks")
+    @RolesAllowed("Employee")
     public ResponseEntity<ResponseWrapper> employeePendingTasks(){
         List<TaskDTO> taskDTOList = taskService.listAllTasksByStatusIsNot(Status.COMPLETE);
 
@@ -56,12 +63,14 @@ public class TaskController {
     }
 
     @PutMapping("/employee/update/")
+    @RolesAllowed("Employee")
     public ResponseEntity<ResponseWrapper> employeeUpdateTask(@RequestBody TaskDTO task){
         taskService.updateStatus(task);
         return ResponseEntity.ok(new ResponseWrapper("Task is updated", HttpStatus.OK));
     }
 
     @GetMapping("/employee/archive")
+    @RolesAllowed("Employee")
     public ResponseEntity<ResponseWrapper> employeeArchivedTasks(){
         List<TaskDTO> taskDTO = taskService.listAllTasksByStatus(Status.COMPLETE);
         return ResponseEntity.ok(new ResponseWrapper("Tasks are retrieve", taskDTO, HttpStatus.OK));
