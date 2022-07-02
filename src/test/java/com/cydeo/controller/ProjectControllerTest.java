c0dfd9b638ad244b36ac8584e1014b3375119226
serikmeiirbek <serik.meiirbek@gmail.com>
2022-07-02 12:01:08 -0400

    Added ProjectControllerTest.java

---
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -0,0 +1,66 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Gender;
+import com.cydeo.enums.Status;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProjectControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    static UserDTO userDTO;
+    static ProjectDTO projectDTO;
+
+    @BeforeAll
+    static void setUp(){
+        userDTO = UserDTO.builder()
+                .id(2L)
+                .firstName("ozzy")
+                .lastName("ozzy")
+                .userName("ozzy")
+                .passWord("abc1")
+                .confirmPassWord("abc1")
+                .role(new RoleDTO(2L, "Manager"))
+                .gender(Gender.MALE)
+                .build();
+
+        projectDTO = ProjectDTO.builder()
+                .projectCode("Api1")
+                .projectName("Api-ozzy")
+                .assignedManager(userDTO)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusDays(5))
+                .projectDetail("Api Test")
+                .projectStatus(Status.OPEN)
+                .build();
+
+    }
+
+    @Test
+    public void givenNoToken_whenGetRequest() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/project"))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+
+}
